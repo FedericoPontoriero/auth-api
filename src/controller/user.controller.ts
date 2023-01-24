7eff20c580fbace5874892faa13c5cf6a095f25e
Federico <federicolucaspontoriero@gmail.com>
2023-01-24 15:49:40 -0300

    added types and controller for registration

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { CreateUserInput } from "../schema/user.schema";
+import { createUser } from "../service/user.service";
+
+export async function creaseUserHandler(
+  req: Request<{}, {}, CreateUserInput>,
+  res: Response
+) {
+  const body = req.body;
+
+  try {
+    const user = await createUser(body);
+
+    return res.send("User successfully created");
+  } catch (e: any) {
+    console.log(e);
+    if (e.code === 11000) {
+      return res.status(409).send("Account already exists");
+    }
+
+    return res.status(500).send(e);
+  }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import validateResource from "../middleware/validateResource";
+import { createUserSchema } from "../schema/user.schema";
+import { creaseUserHandler } from "../controller/user.controller";
 
 const router = express.Router();
 
-router.post("/api/users", (req, res) => res.sendStatus(200));
+router.post(
+  "/api/users",
+  validateResource(createUserSchema),
+  creaseUserHandler
+);
 
 export default router;
