e3cc9b0b83adf168a7c18c572ecd6a614be07173
Federico <federicolucaspontoriero@gmail.com>
2023-01-26 15:37:51 -0300

    jwt configuration

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,8 @@ export default {
   port: 3000,
   dbUri: "mongodb://localhost:27017/auth-api",
   logLevel: "info",
+  accessTokenPrivateKey: "",
+  refreshTokenPrivateKey: "",
   smtp: {
     user: "test@gmail.com",
     pass: "someVeryStrongPassword",
