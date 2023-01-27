8eca1cbc5040bf11213e6434f7dc04f6f342b0e0
Federico <federicolucaspontoriero@gmail.com>
2023-01-27 15:16:44 -0300

    deserializeUser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import config from "config";
 import express from "express";
+import deserializeUser from "./middleware/deserializeUser";
 
 import router from "./routes";
 import connectToDb from "./utils/connectToDb";
@@ -9,6 +10,7 @@ import log from "./utils/logger";
 const app = express();
 
 app.use(express.json());
+app.use(deserializeUser);
 app.use(router);
 
 const port = config.get("port");
